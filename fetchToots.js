fb6aaf3f1c9069825f26fe2af9ef4285ea5156cc
gummipunkt <patrick@gummipunkt.eu>
2023-02-26 11:22:56 +0100

    still in progress

--- fetchToots.js
+++ fetchToots.js
@@ -1,27 +1,32 @@
 // Mastodon
 import dotenv from 'dotenv';
-import masto from './auth.js';
+import mastologin from './auth.js';
+import fs, { stat } from 'fs';
 dotenv.config({ path: './.env' });
 
-let obj = {};
-let jsonData = [];
+const fetchToots = async function() {
+    try {
+    const jsonData = masto.v1.accounts.listStatuses("109279080821915658", {limit: 40})
+    return jsonData;
+    } catch(err) {
+        alert(err)
+    }
+};
 
-const fetchToots = new Promise((resolve, reject) => {
-    masto.v1.accounts.listStatuses("109279080821915658", {limit: 40}).then((data) => {
-        for (let i = 0; i < data.length; i++) {
-            obj = {
-                "tootID": data[i].id,
-                "boosted": data[i].reblogged,
-                "url": data[i].url,
-                "username": data[i].account.username,
-                "avatarStatic": data[i].account.avatarStatic,
-                "createdAt": data[i].account.createdAt,
-                "content": data[i].content
-            };
-            jsonData.push(obj);
-        }
-        resolve(jsonData);
-    }).catch(err => console.log(err));
-});
+const writeJson = async function() {
+    try {
+    const filepathenv = process.env.FILE;
+    const statusfile = fs.writeFile(filepathenv, JSON.stringify(jsonDataFile), { flags: 'w+' }, function (err, jsonDataFile) {
+        console.log('Successfully wrote file');
+        return statusfile;
+    });
+    } catch(err) {
+        alert(err)
+    }
+};
+
+const masto = await mastologin();
+const jsonDataFile = await fetchToots();
+await writeJson();
 
 export default fetchToots;
